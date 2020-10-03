5958d9a8aff4a6c71dfb67b12da83da3412e8acd
Caspero-62 <nwaforuchechukwu2007@gmail.com>
2020-10-03 21:03:08 +0100

    checking second stage work

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,16 +5,15 @@ import {Http, Response} from '@angular/http';
 @Injectable()
 export class ProductService {
 
-  private _albumUrl;
+  private _albumUrl = '../assets/album.json';
 
+  
+  constructor(private _http: Http) {
+  }
+  
   getAlbum(id: number) {
     return this._http.get(this._albumUrl).map(response => (
       response.json()
     ));
   }
-
-  constructor(private _http: Http) {
-    this._albumUrl = '../assets/album.json';
-  }
-
 }
